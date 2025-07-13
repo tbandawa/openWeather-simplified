60c72b4ebe2f406dc18d129ff93df19d78c7496b
Tendai  Bandawa <tonderaibandawa@gmail.com>
2025-07-13 20:57:22 +0200

    test successfull api call

--- app/src/test/java/me/tbandawa/android/openweather/simplified/base/BaseTest.kt
+++ app/src/test/java/me/tbandawa/android/openweather/simplified/base/BaseTest.kt
@@ -1,6 +1,13 @@
 package me.tbandawa.android.openweather.simplified.base
 
+import io.ktor.client.engine.mock.MockEngine
+import io.ktor.client.engine.mock.respond
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.headersOf
+import io.ktor.utils.io.ByteReadChannel
 import kotlinx.serialization.json.Json
+import me.tbandawa.android.openweather.simplified.data.api.OpenWeatherApiClient
 import me.tbandawa.android.openweather.simplified.domain.mapper.FiveDayWeatherResponseMapper
 import org.junit.Before
 import org.junit.runner.RunWith
@@ -11,13 +18,33 @@ abstract class BaseTest {
 
     protected lateinit var fiveDayWeatherMapper: FiveDayWeatherResponseMapper
 
+    protected lateinit var openWeatherApiClient: OpenWeatherApiClient
+
+    protected lateinit var mockEngine: MockEngine
+
     @Before
     fun testsSetup() {
         fiveDayWeatherMapper = FiveDayWeatherResponseMapper()
     }
 
+    protected fun enqueueResponse(
+        fileName: String,
+        statusCode: HttpStatusCode = HttpStatusCode.OK
+    ) {
+        mockEngine = MockEngine { request ->
+            respond(
+                content = ByteReadChannel(this::class.java.classLoader!!.getResource(fileName).readText()),
+                status = statusCode,
+                headers = headersOf(HttpHeaders.ContentType, "application/json")
+            )
+        }
+    }
+
     protected inline fun <reified T : Any> readJsonResponse(fileName: String) : T {
-        val fileContent = this::class.java.classLoader!!.getResource(fileName).readText()
+        val fileContent = stringResponse(fileName)
         return Json.decodeFromString<T>(fileContent)
     }
+
+    protected fun BaseTest.stringResponse(fileName: String): String =
+        this::class.java.classLoader!!.getResource(fileName).readText()
 }
--- app/src/test/java/me/tbandawa/android/openweather/simplified/data/api/OpenWeatherApiClientTest.kt
+++ app/src/test/java/me/tbandawa/android/openweather/simplified/data/api/OpenWeatherApiClientTest.kt
@@ -1,4 +1,20 @@
 package me.tbandawa.android.openweather.simplified.data.api
 
-class OpenWeatherApiClientTest {
+import kotlinx.coroutines.runBlocking
+import me.tbandawa.android.openweather.simplified.base.BaseTest
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
+
+class OpenWeatherApiClientTest: BaseTest() {
+
+    @Test
+    fun `five day weather api request is success`() = runBlocking {
+        enqueueResponse("five_day_weather.json")
+        openWeatherApiClient = OpenWeatherApiClient(mockEngine)
+        val response = openWeatherApiClient.fetchFiveDayWeather(1.0, 2.0)
+        assertThat(response.cod, `is`("200"))
+        assertThat(response.list.size, `is`(40))
+        assertThat(response.city.name, `is`("Johannesburg"))
+    }
 }
